85479c3b9b60b799b3d76e05521c4e9dd67f444d
rosa <antonellirosaria@hotmail.it>
2014-09-22 15:05:22 +0200

    exe007

---
+++ exercices/007/solution007.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+word = "Hello World"
+for i in word:
+    print(i)
+    
+# or simply 
+# print("H\ne\nl\nl\no\n\nW\no\nr\nl\nd")
+
