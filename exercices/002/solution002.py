eb1770f28b0ff3add419ccc433144b14a7e0e356
rosa <antonellirosaria@hotmail.it>
2014-09-22 12:30:55 +0200

    exe002

---
+++ exercices/002/solution002.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+ligne = "AbcdEfghIjklnmOpqrstUvwxyz"
+print(ligne)
+engil = ligne[::-1]
+print(engil)
+
+
+    
