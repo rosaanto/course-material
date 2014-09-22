c0365fb8279f9a10cef49209e96cf83ef4853b72
rosa <antonellirosaria@hotmail.it>
2014-09-22 15:31:54 +0200

    exe009

---
+++ exercices/009/solution009.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+phantom_menace = """Turmoil has engulfed the Galactic Republic. The\
+ taxation of trade routes to outlying star systems is in\
+ dispute. Hoping to resolve the matter with a blockade of deadly\
+ battleships, the greedy Trade Federation has stopped all shipping to\
+ the small planet of Naboo. While the congress of the Republic\
+ endlessly debates this alarming chain of events, the Supreme\
+ Chancellor has secretly dispatched two Jedi Knights, the guardians of\
+ peace and justice in the galaxy, to settle the conflict"""
+
+num_of_world = phantom_menace.split(' ')
+print(len(num_of_world))
+
+
