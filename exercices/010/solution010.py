035ae90e14cd9534d0c7258dcc3c1193f00a94b1
rosa <antonellirosaria@hotmail.it>
2014-09-22 15:43:25 +0200

    exe010

---
+++ exercices/010/solution010.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+for i in range(0,100):
+    line = "Hello World"
+    print(line)
+
+
+# or simply 
+#print("Hello World\n"*100)
+# it prints and it ends to new line
