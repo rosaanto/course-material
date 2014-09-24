a31fa437d4498760738e805d502f1488594155f0
rosaanto <antonellirosaria@hotmail.it>
2014-09-24 16:46:54 +0200

    try exe110 and exe200

--- exercices/110/solution.py
+++ exercices/110/solution.py
@@ -6,21 +6,23 @@ This is a temporary script file.
 """
 
 import sys
-oper = ['+', '-', '*', '/', '%']
+oper = ['+', '-', '*', '/', '%', '^']
 if (len(sys.argv) != 4):
-    print('usage: python3 ./%s a_number (an_operator +-\*) a_number'
-          % sys.argv[0])
+    print('usage: python3 ./solution.py'
+          'a_number (an_operator + -* / % ^) a_number')
 elif((sys.argv[2] not in oper) or (sys.argv[1].count('.')
      or sys.argv[3].count('.')) > 0
      or (sys.argv[2] == '/' and sys.argv[3] == '0')):
     print('input error')
 elif sys.argv[2] == '+':
-    print(int(sys.argv[1])+int(sys.argv[3]))
+    print(int(sys.argv[1]) + int(sys.argv[3]))
 elif sys.argv[2] == '-':
-    print(int(sys.argv[1])-int(sys.argv[3]))
+    print(int(sys.argv[1]) - int(sys.argv[3]))
 elif sys.argv[2] == '*':
-    print(int(sys.argv[1])*int(sys.argv[3]))
+    print(int(sys.argv[1]) * int(sys.argv[3]))
 elif sys.argv[2] == '/':
-    print(int(sys.argv[1])/int(sys.argv[3]))
+    print(int(sys.argv[1]) / int(sys.argv[3]))
 elif sys.argv[2] == '%':
     print(int(sys.argv[1]) % int(sys.argv[3]))
+elif sys.argv[2] == '^':
+    print(int(sys.argv[1]) ** int(sys.argv[3]))
