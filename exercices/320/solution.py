ac726648f6804aa17b3d7843e5601f47bf6e0343
rosaanto <antonellirosaria@hotmail.it>
2014-09-26 19:48:44 +0200

    bye

--- exercices/320/solution.py
+++ exercices/320/solution.py
@@ -10,21 +10,19 @@ words_open = open('words', 'r')
 words_read = words_open.read()
 words = words_read
 # words = ['hello', 'hello', 'hello', 'helllleeeooooeertty']
-
 i = 0
 # input('press to continue')
 
-#print(set(word))
 m = {}
-for l in set(word)-set(('\n',"'")):
-#    my_dict = {'char': l, 'freq': word.count(l)/len(word)}
-    m[l] = word.count(l)/len(word)
+for l in set(words)-set(('\n', "'")):
+    # my_dict = {'char': l, 'freq': words.count(l)/len(words)}
+    m[l] = words.count(l)/len(words)
 # print(sorted(m.items()))
-#print(sorted(m))
+# print(sorted(m))
 for char in sorted(m):
     print("{}: {:.2f}".format(char, m[char]))
 
-
-#for i in sorted(m, key=lambda x: x['char']):
-#    print(m[i]['char']+':',"".format(n[i]['freq']))
- #   print("{char}: {freq:.2f}".format(char=i['char'], freq=i['freq']))
+# ca ne marche plus... tu peux regarder pour le format simplement
+# for i in sorted(m, key=lambda x: x['char']):
+#    print(m[i]['char']+':',"".format(m[i]['freq']))
+#    print("{char}: {freq:.2f}".format(char=i['char'], freq=i['freq']))
